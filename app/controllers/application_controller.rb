74210f4f890ba165f4242741a3c0aa398191c930
Komatsuzaki1935 <69839485+Komatsuzaki1935@users.noreply.github.com>
2020-10-01 17:59:10 +0900

    Basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :furigana_first_name, :furigana_last_name, :birthday])
   end
   def basic_auth
-    authenticate_or_request_with_http_basic do |email, password|
-      email == ENV["BASIC_AUTH_EMAIL"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    authenticate_or_request_with_http_basic do |username, password|
+     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
