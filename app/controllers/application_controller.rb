a1418d9d8c63a445028aba56e6b4bd5bb3c7cf55
Komatsuzaki1935 <69839485+Komatsuzaki1935@users.noreply.github.com>
2020-10-01 17:45:33 +0900

    商品出品機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
-    
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :furigana_first_name, :furigana_last_name, :birthday])
+  end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |email, password|
+      email == ENV["BASIC_AUTH_EMAIL"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
