eb53588499ca86ddcbeffd013f345be661e57b05
Komatsuzaki1935 <69839485+Komatsuzaki1935@users.noreply.github.com>
2020-10-01 15:25:32 +0900

    itemsコントローラー、itemモデル、item単体テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-describe Item do
+RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
@@ -7,7 +7,7 @@ describe Item do
   describe '商品登録' do
 
     context '商品登録がうまくいくとき' do
-      it "商品名、商品説明、カテゴリー、商品状態、配送料、配送元、配送日、料金 が存在すれば登録できる" do
+      it "商品画像、商品名、商品説明、カテゴリー、商品状態、配送料、配送元、配送日、料金 が存在すれば登録できる" do
         expect(@item).to be_valid
       end
       it "カテゴリーを選択すれば登録できる" do
@@ -37,6 +37,11 @@ describe Item do
     end
 
     context '商品登録がうまくいかないとき' do
+      it "商品画像が空では登録できないこと" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
       it "商品名が空では登録できないこと" do
         @item.name = ""
         @item.valid?
@@ -48,54 +53,29 @@ describe Item do
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
       it "カテゴリーを選択しないと登録できないこと" do
-        @item.category_id = ""
-        @item.valid?
-        expect(@item.errors.full_messages).to include("CategoryId Select")
-      end
-      it "category_id = 1 だと登録できないこと" do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("CategoryId Select")
+        expect(@item.errors.full_messages).to include("Category Select")
       end
       it "商品状態を選択しないと登録できないこと" do
-        @item.condition_id = ""
-        @item.valid?
-        expect(@item.errors.full_messages).to include("ConditionId Select")
-      end
-      it "condition_id = 1 だと登録できないこと" do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("ConditionId Select")
+        expect(@item.errors.full_messages).to include("Condition Select")
       end
       it "配送料を選択しないと登録できないこと" do
-        @item.shipping_charge_id = ""
-        @item.valid?
-        expect(@item.errors.full_messages).to include("ShippingChargeId Select")
-      end
-      it "shipping_charge_id = 1 だと登録できないこと" do
         @item.shipping_charge_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("ShippingChargeId Select")
+        expect(@item.errors.full_messages).to include("Shipping charge Select")
       end
       it "配送元を選択しないと登録できないこと" do
-        @item.shipping_from_id = ""
-        @item.valid?
-        expect(@item.errors.full_messages).to include("ShippingFromId Select")
-      end
-      it "shipping_from_id = 1 だと登録できないこと" do
         @item.shipping_from_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("ShippingFromId Select")
+        expect(@item.errors.full_messages).to include("Shipping from Select")
       end
       it "配送日を選択しないと登録できないこと" do
-        @item.shipping_day_id = ""
-        @item.valid?
-        expect(@item.errors.full_messages).to include("ShippingDayId Select")
-      end
-      it "shipping_day_id = 1 だと登録できないこと" do
         @item.shipping_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("ShippingDayId Select")
+        expect(@item.errors.full_messages).to include("Shipping day Select")
       end
       it "料金が空では登録できないこと" do
         @item.price = ""
@@ -112,7 +92,7 @@ describe Item do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Out of setting range")
       end
-      it "料金 が10000000以上だと登録できないこと" do
+      it "料金 が9999999以上だと登録できないこと" do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price Out of setting range")
