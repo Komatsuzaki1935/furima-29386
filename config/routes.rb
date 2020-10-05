ff7bc56152ee81be951d86940979a29075b398f3
Komatsuzaki1935 <69839485+Komatsuzaki1935@users.noreply.github.com>
2020-10-05 13:10:33 +0900

    商品詳細表示機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :show, :new, :create, :edit] do
+  resources :items, only: [:index, :show, :new, :create, :edit, :destroy] do
   end
 end
